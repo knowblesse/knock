734f407091adc83a57fe1ed9f5679c112eaa7642
Knowblesse <knowblesse@gmail.com>
2023-06-21 17:17:31 +0900

    basic working version

---
+++ knock.py
@@ -0,0 +1,23 @@
+import requests
+
+class Knock():
+    def __init__(self):
+        try:
+            with open("credentials.txt", "r") as f:
+                text = f.read().split('\n')
+        except FileNotFoundError:
+            raise(FileNotFoundError("Can not find credential file"))
+        if len(text) != 2:
+            raise(FileNotFoundError('Wrong credentials file'))
+        self.token = text[0]
+        self.chat_id = text[1]
+    def sendMsg(self, obj):
+        sendMessage = str(obj)
+        requests.get(
+            "https://api.telegram.org/"
+            + self.token
+            + "/sendMessage?chat_id="
+            + self.chat_id
+            +"&text="
+            + sendMessage
+        )
